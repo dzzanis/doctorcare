01e1776b68e04e11be11cf85254a6986678838be
Diogo Zanis <zanisdiogo@gmail.com>
2022-06-05 17:35:26 -0300

    Ajuste na função showNavOnScroll

--- main.js
+++ main.js
@@ -1,68 +1,70 @@
 window.addEventListener('scroll', onScroll)
 onScroll()
 
-function onScroll(){
-    showNavOnScroll()
-    showBackToTopButtonOnScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
 
-    activateMenuAtCurrentSection(home)
-    activateMenuAtCurrentSection(services)
-    activateMenuAtCurrentSection(about)
-    activateMenuAtCurrentSection(contact)
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
 
-function showNavOnScroll(){
-    if(scrollY > 0){
-        navigation.classList.add("scroll")
-    } else {
-        navigation.classList.remove("scroll")
-    }
+function showNavOnScroll() {
+  navigation = document.getElementById('navigation')
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
 }
 
-function showBackToTopButtonOnScroll(){
-    if(scrollY > 1450){
-        backToTopButton.classList.add("show")
-    } else {
-        backToTopButton.classList.remove("show")
-    }
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 1450) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
 }
 
-function activateMenuAtCurrentSection(section){
-    const targetLine = scrollY + innerHeight / 2
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
 
-    // verifica se a seção passou da linha alvo
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+  // verifica se a seção passou da linha alvo
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
 
-    //verifica se a base está abaixo da linha alvo
-    const sectionEndsAt = sectionTop + sectionHeight
-    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
-    
-    //limites da seção
-    const sectionBondaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
-    
-    const sectionId = section.getAttribute('id')
-    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+  //verifica se a base está abaixo da linha alvo
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
 
-    menuElement.classList.remove('active')
-    if(sectionBondaries){
-        menuElement.classList.add('active')
-    }
+  //limites da seção
+  const sectionBondaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBondaries) {
+    menuElement.classList.add('active')
+  }
 }
 
-function openMenu(){
-    document.body.classList.add("menu-expanded")
+function openMenu() {
+  document.body.classList.add('menu-expanded')
 }
 
-function closeMenu(){
-    document.body.classList.remove("menu-expanded")
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
 }
 
 ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: '700',
+  origin: 'top',
+  distance: '30px',
+  duration: '700'
 }).reveal(`
     #home, 
     #home img, 
